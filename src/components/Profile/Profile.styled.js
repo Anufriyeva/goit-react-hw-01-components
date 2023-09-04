c532cd686aae6a3910ef5fade190124a22fac04a
Evgeniya Anufriyeva <2281474@gmail.com>
2023-09-04 16:03:55 +0300

    style-commit1

--- src/components/Profile/Profile.styled.js
+++ src/components/Profile/Profile.styled.js
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 export const ProfileWrapper = styled.div`
   /* display: flex;
   flex-direction: row; */
-  background-color: #f0f0f0;
+  background-color: black;
   border-radius: 4px;
-  padding: 50px;
+  padding: 30px;
   width: 300px;
   margin: 20px auto;
 `;
@@ -14,36 +14,38 @@ export const DescriptionWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-right: 20px;
 `;
 
 export const Avatar = styled.img`
   width: 100px;
   height: 100px;
   border-radius: 50%;
+  margin-bottom: 20px;
+
 `;
 
 export const Name = styled.p`
   font-size: 18px;
+  color: #fff;
   font-weight: bold;
   margin: 5px 0;
 `;
 
 export const Tag = styled.p`
   font-size: 14px;
-  color: #666;
+  color: #fff;
   margin: 5px 0;
 `;
 
 export const Location = styled.p`
   font-size: 14px;
-  color: #666;
+  color: #fff;
   margin: 5px 0;
 `;
 
 export const StatsList = styled.ul`
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: space-around;
   align-items: center;
   list-style: none;
   padding: 0;
@@ -53,17 +55,18 @@ export const StatItem = styled.li`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-right: 20px;
+  /* margin-right: 20px; */
 `;
 
 export const StatLabel = styled.span`
   font-size: 14px;
-  color: #666;
+  color: #fff;
   margin: 5px 0;
 `;
 
 export const StatQuantity = styled.span`
   font-size: 16px;
+  color: #fff;
   font-weight: bold;
   margin: 5px 0;
 `;
--- src/components/Statistics/Statistics.styled.js
+++ src/components/Statistics/Statistics.styled.js
@@ -1,38 +1,45 @@
 import styled from 'styled-components';
 
 export const StatisticsWrapper = styled.section`
-  background-color: #f0f0f0;
+  background-color: #000;
   border-radius: 4px;
   padding: 20px;
-  width: 300px;
+  width: 400px;
   margin: 0 auto;
 `;
 
 export const Title = styled.h2`
   font-size: 24px;
   font-weight: bold;
+  color: #fff;
   margin-bottom: 10px;
+  text-align: center;
 `;
 
 export const StatList = styled.ul`
+display: flex;
+  justify-content: space-around;
+  align-items: center;
   list-style: none;
   padding: 0;
 `;
 
 export const StatItem = styled.li`
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: 10px;
 `;
 
 export const Label = styled.span`
   font-size: 18px;
   font-weight: bold;
+  color: #fff;
+  margin-bottom: 10px;
 `;
 
 export const Percentage = styled.span`
   font-size: 16px;
-  color: #666;
+  color: #fff;
 `;
 
